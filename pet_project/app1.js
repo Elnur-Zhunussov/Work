48b176995dc87ad266770e3ee9657ebdfc02861d
Elnur Zhunussov <elnurzhunussov@gmail.com>
2023-09-08 16:58:07 +0600

    pet project

---
+++ pet_project/app1.js
@@ -0,0 +1,41 @@
+const users = [
+    {
+        'username': 'JackBoHorse',
+        'email': 'horseman11@gmail.com',
+        'password': 'ZyeehawpartnerZ',
+        'role': 'user',
+    },
+    {
+        'username': 'lilprince',
+        'email': 'daprince@gmail.com',
+        'password': 'indahood777',
+        'role': 'user',
+    },
+    {
+        'username': 'Simon',
+        'email': 'simonjones@gmail.com',
+        'password': '123Admin123',
+        'role': 'admin',
+    }
+];
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-button");
+
+loginButton.addEventListener("click", validateForm);
+
+function validateForm(e) {
+    e.preventDefault();
+    const enteredUsername = loginForm.username.value;
+    const enteredPassword = loginForm.password.value;
+
+    if (enteredUsername == '' || enteredPassword == '') {
+        alert('Please ensure that both correct username and password are entered')
+    } else {
+        if (users.find(user => user.username === enteredUsername && user.password === enteredPassword)) {
+            alert(`Welcome`);
+        } else {
+            alert('Incorrect username or password');
+        }
+    }
+}
